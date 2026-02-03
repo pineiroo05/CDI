3c9c2fcc2ceb2622ee585988b9a0f4ca5736b869
Enrique Pineiro Lopez <enrique@debian.debian.com>
2026-02-03 21:48:45 +0100

    Practica 2, añadido nuevo paquete para la segunda parte

--- practica2/src/main/java/parte2/Practica2_2.java
+++ practica2/src/main/java/parte2/Practica2_2.java
@@ -1,30 +1,24 @@
-package com.mycompany.practica2;
+package parte2;
 import java.util.Scanner;
 
-public class Practica2 {
+public class Practica2_2 {
     public static void main(String[] args) {
         var scan=new Scanner(System.in);
         System.out.println("Numero de hilos");
         int nHilos=scan.nextInt();
-        
-        /*
-        Thread listaThreads[]=new Thread[nHilos];
-        for(int i=0; i<nHilos; i++){
-            listaThreads[i]=new Thread(new myThread(), ""+i);
-            listaThreads[i].start();
-        }
-        
-        for(int i=0; i<nHilos; i++){
-            try{
-                listaThreads[i].join();
-            }catch(InterruptedException err){}
-        }*/
         
         Thread listaThreads[]=new Thread[nHilos];
         myThread hilosCompartidos=new myThread();
         for(int i=0; i<nHilos; i++){
             listaThreads[i]=new Thread(hilosCompartidos, ""+i);
             listaThreads[i].start();
+        }
+
+        System.out.println("Hilo a interrumpir");
+        int hiloInt=scan.nextInt();
+
+        if(hiloInt>=0 && hiloInt<nHilos){
+            listaThreads[hiloInt].interrupt();;
         }
         
         for(int i=0; i<nHilos; i++){
@@ -47,6 +41,11 @@ class myThread implements Runnable{
     
     public void run(){
         System.out.println("Hello world, I'm the java thread "+Thread.currentThread().getName()+". Valor de mySum: "+getMySum());
+        if(Thread.currentThread().isInterrupted()){
+            System.out.println("Hilo "+Thread.currentThread().getName()+" interrumpido");
+            return;
+        }
+
         for(int i=0; i<10; i++){
             this.mySum.set(getMySum()+1);
             try{
@@ -62,8 +61,7 @@ class myThread implements Runnable{
 }
 
 /*
-Implementar hilos con runnable.
-- Cada hilo un runnable.
-
-ThreadLocal -> con un solo thread puedo hacer que cada uno tenga su propio mySum
+- interrupt() -> mira de interrumpir el hilo.
+- interrupted() -> mira si el hilo ha sido interrumpido.
+- isInterrupted() -> mira si el 
 */
